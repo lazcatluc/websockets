4bc2b662d30d4a21cfeb5c3090686759879af2b4
Catalin Lazar <lazcatluc@gmail.com>
2015-12-06 10:53:22 +0200

    initial commit

---
+++ src/test/java/samples/websocket/ClientConfiguration.java
@@ -0,0 +1,77 @@
+package samples.websocket;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.client.WebSocketConnectionManager;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+
+import samples.websocket.tomcat.client.GreetingService;
+import samples.websocket.tomcat.client.SimpleClientWebSocketHandler;
+import samples.websocket.tomcat.client.SimpleGreetingService;
+
+@Configuration 
+public class ClientConfiguration implements CommandLineRunner {
+	
+	private static final Log LOGGER = LogFactory.getLog(ClientConfiguration.class);
+
+	@Value("${websocket.uri}")
+	private String webSocketUri;
+
+	private final CountDownLatch latch = new CountDownLatch(1);
+
+	private final AtomicReference<String> messagePayload = new AtomicReference<String>();
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("Waiting for response: latch=" + this.getLatch().getCount());
+		if (this.getLatch().await(10, TimeUnit.SECONDS)) {
+			LOGGER.info("Got response: " + this.getMessagePayload().get());
+		}
+		else {
+			LOGGER.info("Response not received: latch=" + this.getLatch().getCount());
+		}
+	}
+
+	@Bean
+	public WebSocketConnectionManager wsConnectionManager() {
+
+		WebSocketConnectionManager manager = new WebSocketConnectionManager(client(),
+				handler(), this.webSocketUri);
+		manager.setAutoStartup(true);
+
+		return manager;
+	}
+
+	@Bean
+	public StandardWebSocketClient client() {
+		return new StandardWebSocketClient();
+	}
+
+	@Bean
+	public SimpleClientWebSocketHandler handler() {
+		return new SimpleClientWebSocketHandler(greetingService(), this.getLatch(),
+				this.getMessagePayload());
+	}
+
+	@Bean
+	public GreetingService greetingService() {
+		return new SimpleGreetingService();
+	}
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+	public AtomicReference<String> getMessagePayload() {
+		return messagePayload;
+	}
+
+}
---
+++ src/test/java/samples/websocket/tomcat/echo/CustomContainerWebSocketsApplicationTests.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package samples.websocket.tomcat.echo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.SocketUtils;
+
+import samples.websocket.ClientConfiguration;
+import samples.websocket.tomcat.SampleTomcatWebSocketApplication;
+import samples.websocket.tomcat.echo.CustomContainerWebSocketsApplicationTests.CustomContainerConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration({ SampleTomcatWebSocketApplication.class, CustomContainerConfiguration.class })
+@WebIntegrationTest
+@DirtiesContext
+public class CustomContainerWebSocketsApplicationTests {
+
+	private static int PORT = SocketUtils.findAvailableTcpPort();
+
+	@Test
+	public void echoEndpoint() throws Exception {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(ClientConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class)
+						.properties("websocket.uri:ws://localhost:" + PORT + "/ws/echo/websocket")
+						.run("--spring.main.web_environment=false");
+		long count = context.getBean(ClientConfiguration.class).getLatch().getCount();
+		AtomicReference<String> messagePayloadReference = context.getBean(ClientConfiguration.class)
+				.getMessagePayload();
+		context.close();
+		assertEquals(0, count);
+		assertEquals("Did you say \"Hello world!\"?", messagePayloadReference.get());
+	}
+
+	@Test
+	public void reverseEndpoint() throws Exception {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(ClientConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class)
+						.properties("websocket.uri:ws://localhost:" + PORT + "/ws/reverse")
+						.run("--spring.main.web_environment=false");
+		long count = context.getBean(ClientConfiguration.class).getLatch().getCount();
+		AtomicReference<String> messagePayloadReference = context.getBean(ClientConfiguration.class)
+				.getMessagePayload();
+		context.close();
+		assertEquals(0, count);
+		assertEquals("Reversed: !dlrow olleH", messagePayloadReference.get());
+	}
+
+	@Configuration
+	protected static class CustomContainerConfiguration {
+
+		@Bean
+		public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+			return new TomcatEmbeddedServletContainerFactory("/ws", PORT);
+		}
+
+	}
+
+}
